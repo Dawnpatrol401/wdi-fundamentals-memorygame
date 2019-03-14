7554065a102afdabc3ff944e311d8ff8fb1630dd
Dawnpatrol401 <45471320+Dawnpatrol401@users.noreply.github.com>
2019-03-14 14:17:13 -0400

    created game logic

---
+++ memory_game/JS/main.js
@@ -0,0 +1,10 @@
+var cardOne = "queen";
+
+var cardTwo = "queen";
+
+var	cardThree = "king";
+
+var carFour = "king";
+
+console.log("User flipped " + cardThree);
+console.log("User flipped " + cardOne);
