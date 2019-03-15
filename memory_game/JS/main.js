96d31cd7a90be5c81ef3678a04b0abd875db442e
Dawnpatrol401 <45471320+Dawnpatrol401@users.noreply.github.com>
2019-03-15 10:53:20 -0400

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,23 +1,44 @@
-var cards = ["queen","queen","king","king"];
+var cards = ["queen","king","queen","king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne)
-
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[1];
-
-cardsInPlay.push(cardTwo);
-
-console.log("user flipped "+cardTwo);
-
-if(cardsInPlay.length === 2){
-	if(cardOne === cardTwo){
+var checkForMatch = function(){
+	if(cardsInPlay[0] === cardsInPlay[1]){
 		alert("you found a match");
 	}else{
 		alert("sorry try again");
 	}
 }
+
+
+var flipCard = function(cardId){
+	console.log("User flipped "+cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
+
+
+
+
+/*
+===================
+This is the old code that it wanted me to delete but I dont know what is going on so im leaving it for now.
+-----------------
+
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne)
+console.log("User flipped " + cardOne);
+
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("user flipped "+cardTwo);
+
+
+
+===========================
+*/
