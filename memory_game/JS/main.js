2d96c66783775bf11f77db138442421e7a07f5f4
Dawnpatrol401 <45471320+Dawnpatrol401@users.noreply.github.com>
2019-03-17 06:31:52 -0400

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -23,46 +23,39 @@ var cards = [
 ];
 
 var cardsInPlay = [];
-
 var checkForMatch = function(){
+	if(cardsInPlay.length === 2){
 	if(cardsInPlay[0] === cardsInPlay[1]){
 		alert("you found a match");
 	}else{
 		alert("sorry try again");
 	}
 }
+};
 
 
-var flipCard = function(cardId){
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
 	console.log("User flipped "+cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+	for(var i=0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
 
 
 
 
-
-/*
-===================
-This is the old code that it wanted me to delete but I dont know what is going on so im leaving it for now.
------------------
-
-
-var cardOne = cards[0];
-cardsInPlay.push(cardOne)
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("user flipped "+cardTwo);
-
-
-
-===========================
-*/
